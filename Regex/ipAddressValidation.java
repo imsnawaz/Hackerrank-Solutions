27b3ea088002d84dbe6205beec3018c49d7a86f0
Sarfaraz Nawaz <51914167+imsnawaz@users.noreply.github.com>
2019-07-05 03:44:05 +0530

    Add files via upload

---
+++ Regex/ipAddressValidation.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class ipAddressValidation {
+
+    public static void main(String[] args) throws IOException{
+        Pattern ip4 = Pattern.compile("^((2[0-4]\\d|25[0-5]|[01]?\\d{0,2})\\.){3}(2[0-4]\\d|25[0-5]|[01]?\\d{0,2})$");
+        Pattern  ip6 = Pattern.compile("^([\\da-f]{1,4}:){7}[\\da-f]{1,4}$");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+        int t = Integer.parseInt(br.readLine());
+        for(int i =0;i<t;i++){
+            String s = br.readLine();
+            if(ip4.matcher(s).matches())
+                sb.append("IPv4\n");
+            else if(ip6.matcher(s).matches())
+                sb.append("IPv6\n");
+            else
+                sb.append("Neither\n");
+        }
+        System.out.println(sb);
+    }
+}
+
